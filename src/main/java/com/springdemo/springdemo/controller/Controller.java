5fac7398d18e0f733b2b2f728bbee6d816e34391
Gokulraj <gokulrajganesan2000@gmail.com>
2021-01-07 16:33:31 +0530

    print hello world in localhost 5000. post method is implemented.

---
+++ src/main/java/com/springdemo/springdemo/controller/Controller.java
@@ -0,0 +1,18 @@
+package com.springdemo.springdemo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String printHelloWorld(){
+        return "Hello World!";
+    }
+
+    @PostMapping("/")
+    public String getFromURL(@RequestBody String content){
+        System.out.println("From URL : "+ content);
+        return "Sucess!";
+    }
+}
