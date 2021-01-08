1f2aa2782b75d0d15f834a7896c13a4a81f9fdf3
Gokulraj <gokulrajganesan2000@gmail.com>
2021-01-08 15:17:25 +0530

    curd logic is added

--- src/main/java/com/springdemo/springdemo/controller/Controller.java
+++ src/main/java/com/springdemo/springdemo/controller/Controller.java
@@ -2,19 +2,19 @@ package com.springdemo.springdemo.controller;
 
 import org.springframework.web.bind.annotation.*;
 
-//@RestController
-@org.springframework.stereotype.Controller
+@RestController
+//@org.springframework.stereotype.Controller
 public class Controller {
 
     @GetMapping("/")
-    @ResponseBody
+//    @ResponseBody
     public String printHelloWorld(){
         return "Hello World!";
     }
 
-//    @PostMapping("/")
-    @RequestMapping(value = "/", method = RequestMethod.POST)
-    @ResponseBody
+    @PostMapping("/")
+//    @RequestMapping(value = "/", method = RequestMethod.POST)
+//    @ResponseBody
     public String getFromURL(@RequestBody String content){
         System.out.println("From URL : "+ content);
         return "Sucess!!";
